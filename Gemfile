af73f3043116dccb2d66891002b2327b4b8e2650
Elizabeth Day <beth@crazydays.org>
2013-09-08 21:08:45 -0400

    add i18n code

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
-gem 'prawn', git: 'git://github.com/prawnpdf/prawn', branch: 'master'
 gem 'rake'
+gem 'i18n'
+gem 'prawn', git: 'git://github.com/prawnpdf/prawn', branch: 'master'
--- src/resume.rb
+++ src/resume.rb
@@ -1,52 +1,30 @@
 require 'prawn'
+require 'i18n'
 
 class Page
   attr_accessor :pdf
 
-  PROFILE_TEXT = "Creative developer who loves to make useful applications " +
-        "in a beautiful way. Eleven years in software and web development " +
-        "in various forms as well as a bachelors degree in Computer Science. " +
-        "Possesses a passion for learning. Communicates well with other " +
-        "departments and co-workers such as customer service and designers."
-  
-  SKILLS = [ ["Ruby", "Regular expressions", "Java"],
-             ["JavaScript/JQuery", "CSS", "Sinatra/Rails"],
-             ["XSLT", "HTML", "PHP"] ]
-
-  JOB_EXPERIENCE = [
-    { position: "IT and Web Development",
-      company: "Focus Marketing, Inc / T. M. Transition Services, LLC",
-      duration: "7/2012 to present",
-      description: [
-        "Updated and maintained mymenulab.com and the Storefront sites used to create and sell menus",
-        "Used Ruby and Sinatra along with MongoDB to create systems to work in conjunction with the Storefront site.",
-        "Created new menu templates in Pageflex Studio."
-      ]},
-    { position: "Software Development Consultant",
-      company: "COCC",
-      duration: "4/2011 to 7/2011",
-      description: [
-        "Developed Java libraries to store financial statements as HTML document.",
-        "Managed XSL and XTHML display issues for customer statements and resolved issues quickly.",
-        "Assisted support staff in the understanding of the OSI DDS/D3 system."
-      ]},
-    { position: "Programming Consultant/Custom Developer",
-      company: "COWWW Software / Open Solutions, Inc",
-      duration: "5/2002 to 5/2009\n12/2009 to 4/2011",
-      description: [
-        "Utilized regular expressions, XSLT, and XHTML to develop displays for financial statements.",
-        "Wrote Java classes to retrieve information from a database for customized marketing displays.",
-        "Created XHTML displays for statements and credit cards of dozens of financial institutions including CU*Answers, COCC, Fidelity, and Partners Credit Union for Disney employees."
-      ]}
-  ]
-
   def initialize(pdf)
-    @pdf         = pdf
+    @pdf = pdf
+    initialize_measurements
+    initialize_images
+    initialize_i18n
+  end
+
+  def initialize_measurements
     @base_height = @pdf.bounds.height
     @base_width  = @pdf.bounds.width
     @y_position  = @base_height
+  end
+
+  def initialize_images
     @background  = File.join(File.dirname(__FILE__), '../assets/parchment2.jpg')
     @divider     = File.join(File.dirname(__FILE__), '../assets/page-divider.png')
+  end
+
+  def initialize_i18n
+    I18n.load_path = Dir['config/locales/*.yml']
+    I18n.backend.load_translations
   end
 
   def add_background
@@ -75,20 +53,20 @@ class Page
   end
 
   def add_name_text
-    @pdf.text "Elizabeth Day", size: 16, style: :bold
+    @pdf.text I18n.t(:name), size: 16, style: :bold
     add_email_address
     add_twitter_handle
   end
 
   def add_email_address
-    @pdf.formatted_text [ { text: "elizabeth.d.day@gmail.com",
-                            link: "mailto:elizabeth.d.day@gmail.com",
+    @pdf.formatted_text [ { text: I18n.t(:email),
+                            link: "mailto:#{I18n.t(:email)}",
                             size: 12, color: "0000AA", styles: [:underline] } ]
   end
 
   def add_twitter_handle
-    @pdf.formatted_text [ { text: "@bsunrise",
-                            link: "https://twitter.com/bsunrise",
+    @pdf.formatted_text [ { text: "@#{I18n.t(:twitter_handle)}",
+                            link: "https://twitter.com/#{I18n.t(:twitter_handle)}",
                             size: 12, color: "0000AA", styles: [:underline] } ]
   end
 
@@ -96,7 +74,7 @@ class Page
     @pdf.bounding_box([(@base_width/2)-30, @y_position], width: @base_width/2) do
       @pdf.move_down 7
       @pdf.font_size 11
-      @pdf.text "616.366.6823\n5621 Roosevelt St\nCoopersville, MI 49404",
+      @pdf.text "#{I18n.t(:phone_no)}\n#{I18n.t(:street_addr)}\n#{I18n.t(:city_state_zip)}",
         align: :right
       adjust_y_position @pdf.bounds.height + 2
     end
@@ -110,27 +88,26 @@ class Page
   end
 
   def add_profile
-    add_label "Profile"
+    add_label I18n.t :profile, scope: [:label]
     @pdf.bounding_box([120, @y_position], width: @base_width - 140) do
-      @pdf.text PROFILE_TEXT
+      @pdf.text I18n.t(:profile)
       adjust_y_position @pdf.bounds.height
     end
   end
 
   def add_skills
-    add_label "Skills"
-    @pdf.bounding_box([120, @y_position + 4],
-        width: @base_width - 140, height: 51) do
-      @pdf.table(SKILLS, column_widths: [130, 130, 100],
+    add_label I18n.t :skills, scope: [:label]
+    @pdf.bounding_box([120, @y_position + 4], width: @base_width - 140, height: 51) do
+      @pdf.table(I18n.t(:skills), column_widths: [130, 130, 100],
                  cell_style: {borders: [], padding: 2 })
       adjust_y_position @pdf.bounds.height
     end
   end
 
   def add_experience
-    add_label "Experience"
+    add_label I18n.t :experience, scope: [:label]
     @pdf.bounding_box([120, @y_position + 4], width: @base_width - 140) do
-      JOB_EXPERIENCE.each do |job|
+      I18n.t(:job_experience).each do |job|
         create_work_experience_table job_name_table(job), job_duties_table(job)
       end
       experience_note
@@ -145,48 +122,44 @@ class Page
 
   def job_name_table job
     @pdf.make_table(
-      [["#{job[:position]}\n#{job[:company]}", job[:duration]]],
+      [["#{job['position']}\n#{job['company']}", job['duration']]],
       column_widths: [(@base_width-140) * 0.75, (@base_width-140) * 0.25],
       cell_style: {borders: [], padding: 3})
   end
 
   def job_duties_table job
     job_duties_table = []
-    job[:description].each { |descrip| job_duties_table << [nbsp, "\u2022", descrip] }
+    job["description"].each { |descrip| job_duties_table << [nbsp, "\u2022", descrip] }
     @pdf.make_table(job_duties_table, cell_style: {borders: {}, padding: 2})
   end
 
   def experience_note
-    @pdf.text "* See <u><color rgb='0000AA'><link href='http://www.linkedin.com/in/elizabethdawnday'>" +
-      "http://www.linkedin.com/in/elizabethdawnday</link></color></u> " +
-      "for a more detailed overview of previous work history ",
-      size: 8, inline_format: true
+    @pdf.text I18n.t(:experience_note), size: 8, inline_format: true
   end
 
   def add_education
-    add_label "Education"
+    add_label I18n.t :education, scope: [:label]
     @pdf.bounding_box([120, @y_position], width: @base_width - 140) do
-      @pdf.text "Bachelor of Science in Computer Science"
-      @pdf.text "Grand Valley State University - May 2001"
+      @pdf.text I18n.t(:college_degree)
+      @pdf.text "#{I18n.t(:university)} - #{I18n.t(:graduation_date)}"
       @pdf.move_down 4
-      @pdf.text "Member of Upsilon Pi Epsilon Honor Society"
+      @pdf.text I18n.t(:honors)
       adjust_y_position @pdf.bounds.height
     end
   end
 
   def add_activities
-    add_label "Other\nActivities"
+    add_label I18n.t :activities, scope: [:label]
     @pdf.bounding_box([120, @y_position], width: @base_width - 140) do
-      @pdf.text "Member of GR Web Development Group\nMember of West Michigan Ruby User Group"
-      @pdf.text "Crafting and Sewing\nMother"
+      I18n.t(:activities).each { |activity| @pdf.text activity }
       adjust_y_position @pdf.bounds.height
     end
   end
 
   def add_references
-    add_label "References"
+    add_label I18n.t :references, scope: [:label]
     @pdf.bounding_box([120, @y_position], width: @base_width - 140) do
-      @pdf.text "Submitted upon request"
+      @pdf.text I18n.t(:references)
       adjust_y_position @pdf.bounds.height
     end
   end
@@ -198,7 +171,7 @@ class Page
   end
 
   def adjust_y_position adjustment
-    @y_position -= adjustment + 7
+    @y_position -= adjustment + 8
   end
 
   def nbsp
